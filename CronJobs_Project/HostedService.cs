25eed7d3a1c260665b8099a8a4d1a12c77ebb2ef
Ricardo Fulgencio Alves <ricardo.alves@socialbank.com.br>
2022-03-28 13:43:38 -0300

    Add project files.

---
+++ CronJobs_Project/HostedService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NCrontab;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CronJob_Project
+{
+    class HostedService : IHostedService
+    {
+        private readonly IConfiguration _configuration;
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            do
+            {
+                await DelayNextExecutation(cancellationToken);
+                Console.WriteLine("METODO TESTE: " + DateTime.Now);
+                //GenerateFile();
+            } while (!cancellationToken.IsCancellationRequested);
+        }
+
+        private async Task DelayNextExecutation(CancellationToken cancellationToken)
+        {
+            try
+            {
+                var expressinCronSchedule = _configuration["CRON_JOB_SCHEDULE"];
+                var _schedule = CrontabSchedule.Parse(expressinCronSchedule, new CrontabSchedule.ParseOptions { IncludingSeconds = true });
+                var nextRun = _schedule.GetNextOccurrence(DateTime.Now);
+                await Task.Delay(Math.Max(0, (int)nextRun.Subtract(DateTime.Now).TotalMilliseconds), cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}, {DateTime.Now}");
+            }
+        }
+
+        public Task StopAsync(CancellationToken stoppingToken)
+        {
+            //TAS.Service.Manager.Main.GetInstance().StopAll();
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ CronJobs_Project/Program.cs
@@ -0,0 +1,33 @@
+﻿using CronJob_Project;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace CronJobs_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hostBuilder = CreateHostBuilder(args);
+            hostBuilder.RunConsoleAsync();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var hostBuilder = Host.CreateDefaultBuilder(args)
+                 .ConfigureServices((context, services) => { services.AddHostedService<HostedService>(); })
+                 .ConfigureAppConfiguration(configuration =>
+                 {
+                     configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                     configuration.AddJsonFile(
+                         $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+                         optional: true);
+                     configuration.AddEnvironmentVariables();
+                 });
+
+            return hostBuilder;
+        }
+    }
+}
